4a081ce43553143ab65fa26a76b6ae89cc2456b3
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-06-28 15:17:19 -0700

    added SQl Display

--- landing_page.php
+++ landing_page.php
@@ -3,7 +3,19 @@ include "head.php";
 $player = DB_getByName($_GET["player"],$conn);
 ?>
 	<body>
+		<h1><?php echo $_GET["player"]?>'s player data</h1>
 <?php 
+	foreach($player as $k => $v ){ 
+		foreach($player[$k] as $kk => $vv){
+			if($kk != "id"){ 
+				$str = "<p class = \"".$kk."\">" . $kk .": <input  name = \"" .$kk."\" value =\"".$vv."\"";
+				$str .= $kk == "Name" ? ("type=\"text\""):("type=\"Number\"");
+				$str .= "/></p>";
+				echo($str);
+			}
+		}	
+	}
+?>
 		<script>
 			$(document).ready(()=>{
 				console.log("ready");
