441862dca27b8904fb5ac481894a3dce4c3bdccb
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-06-27 18:26:32 -0700

    removed the JSON Dependencies from index
    removed the JSON Dependencies from index

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 		<title>index</title>
 	</head>
 	<body>
-		<?php foreach( json_decode(file_get_contents('Players.Json')) as $name => $k){
-			echo($name);
-		}?>
 		<form action = "landing_page.php" method ="get" >
 			<select id="player" name = "player">
 <?php 
