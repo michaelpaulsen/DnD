3611ab7f9e683d2e1bc10586c23a91f0413b02f8
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-06-27 19:27:11 -0700

    fixed the error msg in insert

--- SQLDB.php
+++ SQLDB.php
@@ -9,15 +9,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-
- }
- function DB_insert($arr,$conn){ 
+function DB_insert($arr,$conn){ 
 	if(!isset($arr[7])){
-		echo "<p class = \"error\">not enough prams passed expected 7</p> <br/>";
+		echo "<p class = \"error\">not enough prams passed expected 8</p> <br/>";
 		return false;
 	}
 	$query = "INSERT into players (Name,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,HP,AP) VALUES(?,?,?,?,?,?,?,?,?)";
-	$stmt = $conn->prepare($query);
+	$stmt  = $conn->prepare($query);
 	$stmt->bind_param("siiiiiiii", $val0, $val1, $val2, $val3, $val4,$val5, $val6, $val7,$val7);
 	foreach($arr as $k){ 
 		var_dump($k);
