5ecabdb812ea591ec531c94270fc9dad66a61993
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-06-27 19:35:11 -0700

    add geters for all and by name
    add geters for all and by name

--- SQLDB.php
+++ SQLDB.php
@@ -10,6 +10,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+function DB_getByName($name ){
+	$query = 'select * form players were Name = ?'; 
+	$stmt  = $conn->prepare($query);
+	$stmt->bind_param('s',$name);
+	return $stmt->execute();
+}
+function DB_getAll(){
+	$query = 'select * from players'; 
+	$stmt  = $conn->prepare($query);
+	return $stmt->execute();
+}
 function DB_update($row, $newValue, $conditon){
 	$query = 'UPDATE table_name SET ? = ? were ?'; 
 	$stmt  = $conn->prepare($query);
