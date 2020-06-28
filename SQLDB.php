39438be8581c75b74caa7c99f2ec6a82babaa8ca
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-06-27 19:44:05 -0700

    removed get by name

--- SQLDB.php
+++ SQLDB.php
@@ -10,12 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-function DB_getByName($name ){
-	$query = 'select * form players were Name = ?'; 
-	$stmt  = $conn->prepare($query);
-	$stmt->bind_param('s',$name);
-	return $stmt->execute();
-}
 function DB_getAll(){
 	$query = 'select * from players'; 
 	$stmt  = $conn->prepare($query);
