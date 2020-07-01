c219398fb5b84b6f3768947c5f2fe35b7b92c090
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-07-01 14:38:08 -0700

    gave landing page a form elem

--- landing_page.php
+++ landing_page.php
@@ -4,6 +4,7 @@ $player = DB_getByName($_GET["player"],$conn);
 ?>
 	<body>
 		<h1><?php echo $_GET["player"]?>'s player data</h1>
+		<form id = "form_main">
 <?php 
 	foreach($player as $k => $v ){ 
 		foreach($player[$k] as $kk => $vv){
@@ -16,6 +17,8 @@ $player = DB_getByName($_GET["player"],$conn);
 		}	
 	}
 ?>
+			<button type = "submit" id = "submit_btn" >submit</button>
+		</form>
 		<script>
 			$(document).ready(()=>{
 				console.log("ready");
